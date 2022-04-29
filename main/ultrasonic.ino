6df1497979a5aec98c33fa813b57f747197ca517
Elliott Turner <ertnxt@gmail.com>
2022-04-29 17:07:57 -0500

    ultrasonic takes three measurements and averages

--- main/ultrasonic.ino
+++ main/ultrasonic.ino
@@ -11,23 +11,32 @@ float measure_distance() { // return distance in inches
     unsigned long start_time = micros();
     unsigned long end_time = 0;
 
-    int i, j;
+    float measurements[3] = {0x0};
 
-    digitalWrite(TRIG_PIN, HIGH);
-    while (micros() < start_time + 6) { NOP; };
-    digitalWrite(TRIG_PIN, LOW);
+    int i, j, k;
+    for (k=0; k<3; k++) {
+        digitalWrite(TRIG_PIN, HIGH);
+        while (micros() < start_time + 6) { NOP; };
+        digitalWrite(TRIG_PIN, LOW);
 
-    for (i=0; i<10000; i++) {
-        if (digitalRead(ECHO_PIN)) {
-            start_time = micros();
-            for (j=0; j<10000; j++) {
-                if (!digitalRead(ECHO_PIN)) {
-                    end_time = micros();
-                    break;
+        for (i=0; i<10000; i++) {
+            if (digitalRead(ECHO_PIN)) {
+                start_time = micros();
+                for (j=0; j<10000; j++) {
+                    if (!digitalRead(ECHO_PIN)) {
+                        end_time = micros();
+                        break;
+                    }
                 }
+                break;
             }
-            break;
         }
+        measurements[k] = (float)(end_time - start_time)/148.0;
+        delay(20);
     }
-    return (float)(end_time - start_time)/148.0;
+    float average = 0;
+    for (i=0; i<3; i++) {
+        average += measurements[i];
+    }
+    return average/3.0;
 }
