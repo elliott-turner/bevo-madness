bf68f5727e13166e8c22bde2aa18638846f1480d
Elliott Turner <ertnxt@gmail.com>
2022-05-03 12:55:03 -0500

    use encoder values to make movements more accurate

--- main/motion.ino
+++ main/motion.ino
@@ -12,19 +12,40 @@ void motion_start() {
 }
 
 void move_straight(float dist) {
-    setMotorSpeed(BOTH_MOTORS, SPEED_NORMAL);
+    resetLeftEncoderCnt();
+	resetRightEncoderCnt();
+
+    setMotorSpeed(RIGHT_MOTOR, SPEED_NORMAL);
+    setMotorSpeed(LEFT_MOTOR, SPEED_NORMAL-0.2);
     if (dist > 0) {
         setMotorDirection(BOTH_MOTORS, MOTOR_DIR_FORWARD);
     }
     else {
         setMotorDirection(BOTH_MOTORS, MOTOR_DIR_BACKWARD);
     }
-    delay(abs(dist)*100.0);
-    move_stop();
+
+    int num_steps = (int)(dist*40.0);
+    bool left_done = false;
+    bool right_done = false;
+    while (true) {
+        if (getEncoderLeftCnt() > num_steps) {
+            left_done = true;
+            setMotorSpeed(LEFT_MOTOR, 0);
+        }
+        if (getEncoderRightCnt() > num_steps) {
+            right_done = true;
+            setMotorSpeed(RIGHT_MOTOR, 0);
+        }
+        if (left_done && right_done) { break; }
+    }
 }
 
 void move_turn(float deg) {
-    setMotorSpeed(BOTH_MOTORS, SPEED_NORMAL);
+    resetLeftEncoderCnt();
+	resetRightEncoderCnt();
+
+    setMotorSpeed(RIGHT_MOTOR, SPEED_NORMAL);
+    setMotorSpeed(LEFT_MOTOR, SPEED_NORMAL-0.2);
     if (deg > 0) {
         setMotorDirection(RIGHT_MOTOR, MOTOR_DIR_BACKWARD);
         setMotorDirection(LEFT_MOTOR, MOTOR_DIR_FORWARD);
@@ -33,8 +54,21 @@ void move_turn(float deg) {
         setMotorDirection(RIGHT_MOTOR, MOTOR_DIR_FORWARD);
         setMotorDirection(LEFT_MOTOR, MOTOR_DIR_BACKWARD);
     }
-    delay(abs(deg)*11.0);
-    move_stop();
+    
+    int num_steps = (int)(deg*2.05);
+    bool left_done = false;
+    bool right_done = false;
+    while (true) {
+        if (getEncoderLeftCnt() > num_steps) {
+            left_done = true;
+            setMotorSpeed(LEFT_MOTOR, 0);
+        }
+        if (getEncoderRightCnt() > num_steps) {
+            right_done = true;
+            setMotorSpeed(RIGHT_MOTOR, 0);
+        }
+        if (left_done && right_done) { break; }
+    }
 }
 
 void move_stop(void) {
