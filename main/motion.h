c535466b58a61679bdb116137dda789405a6041c
Elliott Turner <ertnxt@gmail.com>
2022-05-04 18:17:07 -0500

    add PID line following

--- main/motion.h
+++ main/motion.h
@@ -9,8 +9,13 @@
 #define MOTOR_GAIN_I 0.008
 #define MOTOR_GAIN_D 0.002
 
+#define LINE_GAIN_P 8
+#define LINE_GAIN_I 0.001
+#define LINE_GAIN_D 0
+
 void move_straight(float dist);
 void move_turn(float deg);
 void move_stop(void);
 bool check_for_line(void);
 void move_to_line(void);
+void follow_line_to_intersection(float dist);
--- main/motion.ino
+++ main/motion.ino
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "SimpleRSLK.h"
 
-float line_position = 0;
+double line_position = 0;
 
 uint16_t sensor_vals[LS_NUM_SENSORS];
 
@@ -11,11 +11,12 @@ struct PID_Data {
     double p_error;
     double i_error;
     double d_error;
-    unsigned long last_encoder;
+    unsigned long last_input;
 };
 
 struct PID_Data pid_data_l = {0x0};
 struct PID_Data pid_data_r = {0x0};
+struct PID_Data line_pid_data = {0x0};
 
 void motion_start() {
     line_position = 0;
@@ -158,10 +159,118 @@ void move_to_line(float dist) {
     move_stop();
 }
 
+void follow_line_to_intersection(float dist) {
+    resetLeftEncoderCnt();
+	resetRightEncoderCnt();
+
+    setMotorSpeed(BOTH_MOTORS, SPEED_NORMAL);
+    setMotorDirection(BOTH_MOTORS, MOTOR_DIR_FORWARD);
+
+    bool at_intersection = false;
+    bool at_line = false;
+    double previous_line_position = 0;
+    int j;
+
+    int num_steps = (int)(abs(dist)*40.0);
+    bool left_done = false;
+    bool right_done = false;
+    pid_start();
+    while (true) {
+        readLineSensor(sensor_vals);
+        at_intersection = true;
+        for (j=0; j<LS_NUM_SENSORS; j++) {
+            if (sensor_vals[j] < BLACK_VAL) {
+                at_intersection = false;
+                break;
+            }
+        }
+
+        at_line = false;
+        for (j=0; j<LS_NUM_SENSORS; j++) {
+            if (sensor_vals[j] >= BLACK_VAL) {
+                at_line = true;
+                break;
+            }
+        }
+
+        if (at_intersection) {
+            if (getEncoderLeftCnt() < getEncoderRightCnt()) { num_steps = getEncoderLeftCnt(); }
+            else { getEncoderLeftCnt(); }
+        }
+
+        // estimate line position using center of mass equation
+        line_position = (
+                1.00 * sensor_vals[0] + 
+                0.71 * sensor_vals[1] +
+                0.43 * sensor_vals[2] +
+                0.14 * sensor_vals[3] +
+                -0.14 * sensor_vals[4] +
+                -0.43 * sensor_vals[5] +
+                -0.71 * sensor_vals[6] +
+                -1.00 * sensor_vals[7]
+            ) / (
+                sensor_vals[0] +
+                sensor_vals[1] +
+                sensor_vals[2] +
+                sensor_vals[3] +
+                sensor_vals[4] +
+                sensor_vals[5] +
+                sensor_vals[6] +
+                sensor_vals[7]
+            );
+
+        if (at_line) { previous_line_position = line_position; }
+        else { line_position = previous_line_position; }
+
+        if (getEncoderLeftCnt() > num_steps) {
+            left_done = true;
+            setMotorSpeed(LEFT_MOTOR, 0);
+        }
+        if (getEncoderRightCnt() > num_steps) {
+            right_done = true;
+            setMotorSpeed(RIGHT_MOTOR, 0);
+        }
+        if (millis() - last_time >= 100) {
+            curr_time = (double)millis();
+            elap_time = curr_time - last_time;
+
+            line_pid_data.d_error = (line_position - line_pid_data.p_error) / elap_time;
+            line_pid_data.p_error = line_position;
+            line_pid_data.i_error += line_pid_data.p_error * elap_time;
+            double response = 
+                line_pid_data.p_error * LINE_GAIN_P +
+                line_pid_data.i_error * LINE_GAIN_I +
+                line_pid_data.d_error * LINE_GAIN_D;
+
+            Serial.println(line_pid_data.p_error);
+            Serial.println(line_pid_data.i_error);
+            Serial.println(line_pid_data.d_error);
+            Serial.println(line_position);
+            Serial.println(response);
+            Serial.println();
+
+            double speed_l = SPEED_NORMAL - (int)response;
+            double speed_r = SPEED_NORMAL + (int)response;
+
+            if (speed_l < 0) { speed_l = 0; }
+            if (speed_r < 0) { speed_r = 0; }
+            if (speed_l > 20) { speed_l = 20; }
+            if (speed_r > 20) { speed_r = 20; }
+
+            if (left_done && !right_done) { pid_step(0, speed_r); }
+            else if (!left_done && right_done) { pid_step(speed_l, 0); }
+            else { pid_step(speed_l, speed_r); }
+        }
+        if (left_done && right_done) { break; }
+    }
+    move_stop();
+}
+
 void pid_start() {
     last_time = millis();
     memset(&pid_data_l, 0x0, sizeof(pid_data_l));
     memset(&pid_data_r, 0x0, sizeof(pid_data_r));
+    memset(&line_pid_data, 0x0, sizeof(line_pid_data));
 }
 
 void pid_step(float set_l, float set_r) {
@@ -170,8 +279,8 @@ void pid_step(float set_l, float set_r) {
 
     double error, response, steps;
 
-    steps = (double)getEncoderLeftCnt() - pid_data_l.last_encoder;
-    pid_data_l.last_encoder += steps;
+    steps = (double)getEncoderLeftCnt() - pid_data_l.last_input;
+    pid_data_l.last_input += steps;
     error = steps - set_l * elap_time * 0.0175;
     error = -1*error;
     pid_data_l.d_error = (error - pid_data_l.p_error) / elap_time;
@@ -182,9 +291,9 @@ void pid_step(float set_l, float set_r) {
                 pid_data_l.d_error * MOTOR_GAIN_D;
     setMotorSpeed(LEFT_MOTOR, response / elap_time / 0.0175);
 
-    steps = (double)getEncoderRightCnt() - pid_data_r.last_encoder;
-    pid_data_r.last_encoder += steps;
-    error = steps - set_l * elap_time * 0.0175;
+    steps = (double)getEncoderRightCnt() - pid_data_r.last_input;
+    pid_data_r.last_input += steps;
+    error = steps - set_r * elap_time * 0.0175;
     error = -1*error;
     pid_data_r.d_error = (error - pid_data_r.p_error) / elap_time;
     pid_data_r.p_error = error;
